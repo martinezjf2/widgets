b0abae4468c598f874cfdd7544b9086178a6f1ed
Jeffrey Martinez <martinezjf2@gmail.com>
2021-04-18 01:02:53 -0400

    added the accordion component, passed down items as props, and returned the number of items within the array

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import Accordion from './Accordion'
 // eslint-disable-next-line
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end javascript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favorite JS Library among engineers'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating components'
+    }
+]
 export default () => {
-    return <h1>Widgets App!</h1>
+    return <div><Accordion items={items}/></div>
 }
