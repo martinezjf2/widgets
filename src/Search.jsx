f43854bd42a6a39dbfdbb747b76cb3e413260b10
Jeffrey Martinez <martinezjf2@gmail.com>
2021-05-06 17:00:22 -0400

    added useState on the search component

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=SEARCHTERM
 
 const Search = () => {
+    const [term, setTerm] = useState('')
     return <h1>Search</h1>
 }
 
