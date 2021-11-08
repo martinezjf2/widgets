cef0969a36b6ae9003ae90d0d3a842ed9ae5372a
Jeffrey Martinez <martinezjf2@gmail.com>
2021-11-08 18:57:54 -0500

    console log, my data from the response

--- src/Search.jsx
+++ src/Search.jsx
@@ -4,14 +4,27 @@ import axios from 'axios'
 // https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=SEARCHTERM
 
 const Search = () => {
-    const [term, setTerm] = useState('')
+    const [term, setTerm] = useState('programming')
+    const [results, setResults] = useState([])
  
+    console.log(results)
 
 useEffect(() => {
-   const search = async() => {
-    await axios.get("en.wikipedia.org/w/api/php?action=query&list=search&format=json&origin=*&srsearch=programming") 
+   const search = async () => {
+    const { data } = await axios.get("https://en.wikipedia.org/w/api.php?", {
+        params: {
+            action: 'query',
+            list: 'search',
+            origin: '*',
+            format: 'json',
+            srsearch: term,
+        }
+    }); 
+    setResults(data.query.search)
    };
-   search();
+//    if (term) { 
+       search()
+    // }
 }, [term])
 
     return (
